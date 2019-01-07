157e822ad1d8111e9d8b5772bc26c0afa4a705e2
jdeck <jeremias.deck@stud.htwk-leipzig.de>
2019-01-07 12:40:22 +0100

    outputs every span.

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 const util = require('util');
+const DOMParser = require('xmldom').DOMParser;
 
 const readFile = util.promisify(fs.readFile);
 const parseString = util.promisify(xml2js.parseString);
+const parser = new DOMParser();
 
 async function readFodtFile() {
     const data = await readFile('Modulhandbuecher.fodt');
     const xml = data.toString();
     const result = await parseString(xml);
-    return result;
+    const xmlDoc = parser.parseFromString(xml,"text/xml");
+    return xmlDoc;
 }
 
 async function run (){
     const result = await readFodtFile();
-    //console.log(result);
-    const modules = result['office:document']['office:body'][0]['office:text'][0]['table:table'];
-    console.log(modules);
-    modules.forEach(module => {
-        console.log(module['$']);
-    });
+    const tables = result.getElementsByTagName('table:table');
+    
+    for (let i = 0; i < tables.length; i++) {
+        const table = tables[i];
+        const tableRows = table.getElementsByTagName('table:table-row');
+        for (let j = 0; j < tableRows.length; j++) {
+            const tableRow = tableRows[j];
+            const tableCells = tableRow.getElementsByTagName('table:table-cell');
+            for (let k = 0; k < tableCells.length; k++) {
+                const tableCell = tableCells[k];
+                const textPs = tableCell.getElementsByTagName('text:p');
+                for (let m = 0; m < textPs.length; m++) {
+                    const textP = textPs[m];
+                    const spans = textP.getElementsByTagName('text:span');
+                    for (let n = 0; n < spans.length; n++) {
+                        console.log(spans[n]['lastChild']['data']);
+                    }
+                }
+            }
+        }
+    } 
 }
 
 run();
