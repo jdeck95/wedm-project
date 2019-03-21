9e1afa8cf33499dbf8962915788a81b63c93c1d4
jdeck <jerredeck@gmail.com>
2019-03-21 16:53:05 +0100

    generated files are getting deleted now before creating new ones

--- createFile.js
+++ createFile.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 const asyncForEach = require('./helper');
+const rimraf = require("rimraf");
 
 const appendFile = util.promisify(fs.appendFile);
+const mkdirSync = util.promisify(fs.mkdirSync);
 
 const createFile = async function(sortedData, filename) {
     let cellLength = [];
     filename = filename.trim().split(' ').join('_');
+
+    rimraf.sync("./zim");
+    if (!fs.existsSync('./zim')){
+        await fs.mkdirSync('./zim');
+    }
+
     await asyncForEach(sortedData,async (cell) => {
         let items = cell['header'];
         cellLength.push(cell['items'].length);
